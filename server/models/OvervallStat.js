f6d6bc00b2157fe9ceb56bab71cc556d1fc6bdb1
elysee <mboussaemmanuelito@gmail.com>
2023-11-11 15:47:06 +0100

    NEXT monthly feature

--- server/models/OvervallStat.js
+++ server/models/OvervallStat.js
@@ -15,7 +15,7 @@ const OvervallStatSchema = new mongoose.Schema(
     ],
     dailyData: [
       {
-        data: String,
+        date: String,
         totalSales: Number,
         totalUnits: Number,
       },
